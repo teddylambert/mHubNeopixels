58ee82094a720df03a0d795a264b886d33dff700
Teddy Lambert <tclambert00@gmail.com>
2017-07-27 16:17:36 -0500

    Create RibbonLEDs.ino
    Prototype version of ribbon plaque fire effect

---
+++ RibbonLEDs.ino
@@ -0,0 +1,83 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+//  The overall fire brightness
+//  (this can affect both color levels and power consumption)
+int brightness = 225;
+
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
+
+//  The flame color array (the first is the default):
+RGB flameColors[] = {
+  { 255, 95, 0},  // Orange flame
+  { 226, 15, 30}    // Red flame
+  };
+
+//  Number of flame colors
+int NUMBER_OF_COLORS = sizeof(flameColors) / sizeof(RGB);
+
+//  Tracks the current color
+int currentColorIndex = 0;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(74, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+// For the ultimate NeoPixel guide, check out:
+// https://learn.adafruit.com/adafruit-neopixel-uberguide/overview
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(brightness);
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  //  Get the current colors based on the color index:
+  RGB currentColor = flameColors[currentColorIndex];
+
+ //  Flicker, based on our initial RGB values
+  for(int i=4; i<13; i++) { 
+    int flicker = random(0,85);
+    int r1 = currentColor.r-flicker;
+    int g1 = currentColor.g-flicker;
+    int b1 = currentColor.b-flicker;
+    if(g1<0) g1=0;
+    if(r1<0) r1=0;
+    if(b1<0) b1=0;
+    strip.setPixelColor(i,r1,g1, b1);
+  }
+ /* for(int g=18; g<27; g++) {
+    int flicker = random(0,55);
+    int r1 = currentColor.r-flicker;
+    int g1 = currentColor.g-flicker;
+    int b1 = currentColor.b-flicker;
+    if(g1<0) g1=0;
+    if(r1<0) r1=0;
+    if(b1<0) b1=0;
+    strip.setPixelColor(g,r1,g1, b1);
+  }
+ */ 
+  strip.show();
+
+  //  Adjust the delay here, if you'd like.  Right now, it randomizes the 
+  //  color switch delay to give a sense of realism
+  delay(random(10,45));
+}
