74ed550a7670aaf3065d6770c2d7f530b3cddb74
Teddy Lambert <tclambert00@gmail.com>
2017-07-28 14:22:10 -0500

    Increased readability

--- RibbonLEDs.ino
+++ RibbonLEDs.ino
@@ -7,7 +7,6 @@
 #define N_LEDS 74
 
 //  The overall fire brightness
-//  (this can affect both color levels and power consumption)
 int brightness = 225;
 
 struct RGB {
@@ -25,21 +24,7 @@ RGB flameColors[] = {
 //  Tracks the current color
 int currentColorIndex = 0;
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
-
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
-// For the ultimate NeoPixel guide, check out:
-// https://learn.adafruit.com/adafruit-neopixel-uberguide/overview
 
 void setup() {
   strip.begin();
@@ -51,7 +36,7 @@ void loop() {
   //  Get the current colors based on the color index:
   RGB currentColor = flameColors[currentColorIndex];
 
- //  Flicker, based on our initial RGB values
+ //  Flicker, based on our initial RGB values. Check i=9; i<29 when setting up actual sign
   for(int i=9; i<29; i++) { 
     int flicker = random(0,85);
     int r1 = currentColor.r-flicker;
@@ -62,17 +47,7 @@ void loop() {
     if(b1<0) b1=0;
     strip.setPixelColor(i,r1,g1, b1);
   }
- /* for(int g=18; g<27; g++) {
-    int flicker = random(0,55);
-    int r1 = currentColor.r-flicker;
-    int g1 = currentColor.g-flicker;
-    int b1 = currentColor.b-flicker;
-    if(g1<0) g1=0;
-    if(r1<0) r1=0;
-    if(b1<0) b1=0;
-    strip.setPixelColor(g,r1,g1, b1);
-  }
- */ 
+ 
   strip.show();
 
   //  Adjust the delay here, if you'd like.  Right now, it randomizes the 
