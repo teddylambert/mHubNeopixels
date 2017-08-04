c64908fd511f0ad10e7e56b8cc3a935a512b47e3
Teddy Lambert <tclambert00@gmail.com>
2017-08-04 14:26:21 -0500

    Implemented brightnesss

--- RibbonLEDs.ino
+++ RibbonLEDs.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 #define leftPin 5
@@ -8,7 +8,7 @@
 #define stripLength 6
 
 //  The overall fire brightness
-int brightness = 225;
+const int brightness = 225;
 
 struct RGB {
   byte r;
@@ -20,24 +20,26 @@ struct RGB {
 RGB flameColors[] = {
   { 255, 95, 0},  // Orange flame
   { 226, 15, 30}    // Red flame
-  };
+};
 
 //  Tracks the current color
-int currentColorIndex = 0;
+const int currentColorIndex = 0;
 
 Adafruit_NeoPixel leftStrip = Adafruit_NeoPixel(stripLength, leftPin, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel rightStrip = Adafruit_NeoPixel(stripLength, rightPin, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-   #if defined (__AVR_ATtiny85__)
+
+  #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
   #endif
+
   leftStrip.begin();
-  leftStrip.setBrightness(225);
-  leftStrip.show();
   rightStrip.begin();
-  rightStrip.setBrightness(225);
+  leftStrip.setBrightness(brightness);
+  rightStrip.setBrightness(brightness);
+  leftStrip.show();
   rightStrip.show();
 }
 
@@ -45,22 +47,22 @@ void loop() {
   //  Get the current colors based on the color index:
   RGB currentColor = flameColors[currentColorIndex];
 
- //  Flicker, based on our initial RGB values. Check i=9; i<29 when setting up actual sign
-  for(int i=0; i<stripLength; i++) { 
-    int flicker = random(0,85);
-    int r1 = currentColor.r-flicker;
-    int g1 = currentColor.g-flicker;
-    int b1 = currentColor.b-flicker;
-    if(g1<0) g1=0;
-    if(r1<0) r1=0;
-    if(b1<0) b1=0;
-    leftStrip.setPixelColor(i,r1,g1, b1);
-    rightStrip.setPixelColor(i,r1,g1, b1);
+  //  Flicker, based on our initial RGB values. 
+  for (int i = 0; i < stripLength; i++) {
+    int flicker = random(0, 85);
+    int r1 = currentColor.r - flicker;
+    int g1 = currentColor.g - flicker;
+    int b1 = currentColor.b - flicker;
+    if (g1 < 0) g1 = 0;
+    if (r1 < 0) r1 = 0;
+    if (b1 < 0) b1 = 0;
+    leftStrip.setPixelColor(i, r1, g1, b1);
+    rightStrip.setPixelColor(i, r1, g1, b1);
   }
- 
+
   leftStrip.show();
   rightStrip.show();
-  //  Adjust the delay here, if you'd like.  Right now, it randomizes the 
+  //  Adjust the delay here, if you'd like.  Right now, it randomizes the
   //  color switch delay to give a sense of realism
-  delay(random(10,45));
+  delay(random(10, 45));
 }
